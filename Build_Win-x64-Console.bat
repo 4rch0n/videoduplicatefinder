1a6af06b6e2037b8752024675a3603e8abac7665
4rch0n <4rch0n.pl@gmail.com>
2020-08-19 16:20:43 +0200

    Bump version of netcoreapp in Windows build scripts to v3.1; Bump version of MahApps.Metro to v2.2.0; Cleanup Windows build script; Add seperate build scripts for App and Console (win x64)

---
+++ Build_Win-x64-Console.bat
@@ -0,0 +1,14 @@
+@echo off
+
+CD /D VideoDuplicateFinder.Console
+dotnet publish -c Release -v q --self-contained -r win-x64 -f netcoreapp3.1 -o "..\Releases\VDF.Windows-x64"
+CD ..
+
+if not exist ".\Releases\ffmpeg\" goto end
+echo Copy ffmpeg binaries
+if not exist ".\Releases\VDF.Windows-x64\bin" mkdir ".\Releases\VDF.Windows-x64\bin"
+xcopy /q /y ".\Releases\ffmpeg\x64\*.*" ".\Releases\VDF.Windows-x64\bin"
+
+:end
+
+echo Build succeed
